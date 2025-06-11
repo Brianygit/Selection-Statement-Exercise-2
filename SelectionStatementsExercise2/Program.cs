f09bf2e909201ae7dea75642f1a42d0f923ab077
Brian Yates <briany735@gmail.com>
2025-06-11 12:40:53 -0700

    bugs fixed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Runtime.InteropServices;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
@@ -6,30 +8,31 @@
         {
             Console.WriteLine(
                 "Hello! Out of Math, English, Art, Science, History, and Computers. Which is your favorite?");
-            string class = Console.ReadLine();
-            
-            case "Math":
+            string class1 = Console.ReadLine();
+            switch (class1.ToLower())
+            {
+                case "math":
                 Console.WriteLine("Math, its okay.");
                 break;
-            case "English":
+                case "english":
                 Console.WriteLine("English, its okay.");
                 break;
-            case "Art":
-                Console.WriteLine("Art, its okay.");    
+                case "art":
+                Console.WriteLine("Art, its okay.");
                 break;
-            case "Science":
+                case "science":
                 Console.WriteLine("Science is great.");
                 break;
-            case "History":
+                case "history":
                 Console.WriteLine("History is great.");
                 break;
-            case "Computers":
-                Console.WriteLine("Computers, you know it!");       
+                case "computers":
+                Console.WriteLine("Computers, you know it!");
                 break;
-            default:
+                default:
                 Console.WriteLine("Please enter a valid option, please try again. Recess is not an option.");
                 break;
-
+            }
 
         }
     }
--- SelectionStatementsExercise2/obj/Debug/net9.0/SelectionStatementsExercise2.AssemblyInfo.cs
+++ SelectionStatementsExercise2/obj/Debug/net9.0/SelectionStatementsExercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+581dc19c0f4a0bed99ad22d4db0c6c46a40b11c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+013e851881256751d82d0fa08e87962f33092adf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelectionStatementsExercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
