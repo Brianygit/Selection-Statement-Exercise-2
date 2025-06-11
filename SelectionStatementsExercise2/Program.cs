013e851881256751d82d0fa08e87962f33092adf
Brian Yates <briany735@gmail.com>
2025-06-11 10:34:31 -0700

    exercise number 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(
+                "Hello! Out of Math, English, Art, Science, History, and Computers. Which is your favorite?");
+            string class = Console.ReadLine();
+            
+            case "Math":
+                Console.WriteLine("Math, its okay.");
+                break;
+            case "English":
+                Console.WriteLine("English, its okay.");
+                break;
+            case "Art":
+                Console.WriteLine("Art, its okay.");    
+                break;
+            case "Science":
+                Console.WriteLine("Science is great.");
+                break;
+            case "History":
+                Console.WriteLine("History is great.");
+                break;
+            case "Computers":
+                Console.WriteLine("Computers, you know it!");       
+                break;
+            default:
+                Console.WriteLine("Please enter a valid option, please try again. Recess is not an option.");
+                break;
+
+
         }
     }
 }
